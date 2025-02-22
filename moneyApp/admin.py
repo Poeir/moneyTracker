334dec1eee23c88829b76c7feed06ba268296535
Poori Charicha <phuri.c@kkumail.com>
2025-02-22 14:18:43 +0700

    finalCommit

--- moneyApp/admin.py
+++ moneyApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from moneyApp.models import Statement
+
+class statementAdmin(admin.ModelAdmin):
+    list_display = ['name','amount','catagory']
+    list_per_page = 10
+    list_editable = ['amount','catagory']
+    list_filter = ['catagory','amount']
+    search_fields = ['name']
+    fields = ['catagory','name','amount']
 
 # Register your models here.
+admin.site.register(Statement,statementAdmin)
--- moneyApp/models.py
+++ moneyApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Statement(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+
+    choice = (
+        ('income','รายรับ'),
+        ('expense','รายจ่าย')
+    )
+
+    catagory = models.CharField(max_length=10,choices=choice,default=1)
+    
+    def __str__(self):
+        return self.name + "|" + str(self.amount) + "|" + self.catagory
